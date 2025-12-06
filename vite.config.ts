7b99b9c00ebb64a11fa068b184dc28785ff1e871
Cactus Dev <cactutechdesenvolvimento@gmail.com>
2025-12-06 14:14:49 -0300

    Change base path for Vercel deploymen
    Updated base path for deployment to Vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: O nome do repositório deve estar entre barras
-  base: '/LinkTree-Personalizado-Festa-dos-Doces/',
+  // Para Vercel, o base deve ser '/' (raiz)
+  base: '/',
 })
